f7bae027c5d672a47991526ee93bc030c4ffe89c
ShanderB <s.hander8910@hotmail.com>
2024-08-05 16:36:56 -0300

    Criado corpo principal do componente.

--- desafio-versao-3/heatmap/src/app/app.component.ts
+++ desafio-versao-3/heatmap/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoadJsonService } from './load-json/load-json.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FilterJsonDataService } from './filter-json-data/filter-json-data.service';
+import { CalculateCentroideService } from './centroide/centroide';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule, CommonModule],
+  providers: [
+    LoadJsonService,
+    FilterJsonDataService,
+    CalculateCentroideService,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'heatmap';
+  data: { object: string; x: number; y: number; }[] = [];
+
+  constructor(
+    private readonly loadJsonService: LoadJsonService,
+    private readonly filterJsonDataService: FilterJsonDataService
+  ) {}
+
+  ngOnInit() {
+    this.loadJsonService
+      .fetchJson('assets/response.json')
+      .subscribe((response) => {
+        this.data = this.filterJsonDataService.filterJson(response);
+
+        console.log(this.data);
+      });
+  }
 }
