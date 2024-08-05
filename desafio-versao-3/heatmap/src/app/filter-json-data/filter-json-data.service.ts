16971140d12d712f866fc2d727d8ab8ce6a8b834
ShanderB <s.hander8910@hotmail.com>
2024-08-05 16:34:21 -0300

    Criado service de filtrar o json. Essa função é responsável por pegar todos os dados do Json, calcular a centroide e adicionar qual o tipo do objeto.

---
+++ desafio-versao-3/heatmap/src/app/filter-json-data/filter-json-data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { CalculateCentroideService } from '../centroide/centroide';
+
+interface DeepstreamMsg {
+  hits: { hits: [{ fields: { 'deepstream-msg': string[] } }] };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilterJsonDataService {
+  foundObjects = new Set<string>();
+  constructor(
+    private readonly calculateCentroideService: CalculateCentroideService
+  ) {}
+
+  filterJson(data: DeepstreamMsg): any[] {
+    return data.hits.hits.flatMap((msg) => {
+      return msg.fields['deepstream-msg'].map((deepstreamMsg: string) => {
+        const [trackingId, xMin, yMin, xMax, yMax, deepstreamObject] =
+          deepstreamMsg.split('|');
+        this.foundObjects.add(deepstreamObject);
+
+        return {
+          ...this.calculateCentroideService.calculateCentroide(
+            +xMin,
+            +xMax,
+            +yMin,
+            +yMax
+          ),
+          object: deepstreamObject,
+        };
+      });
+    });
+  }
+}
